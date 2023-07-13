83eab6cc4cd4014b3eaa4446dbe0a2c732a0641a
lianafinaritra <hei.lianafinaritra@gmail.com>
2023-07-13 21:10:36 +0300

    feat: update mapper

--- src/main/java/com/web/prog4webapp/controller/rest/RestEmployee.java
+++ src/main/java/com/web/prog4webapp/controller/rest/RestEmployee.java
@@ -1,17 +1,13 @@
 package com.web.prog4webapp.controller.rest;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.time.LocalDate;
 
-@AllArgsConstructor
 @Getter
 @Setter
-@NoArgsConstructor
+@Builder
 public class RestEmployee{
     private String lastName;
     private String firstName;
--- src/main/java/com/web/prog4webapp/mapper/EmployeeMapper.java
+++ src/main/java/com/web/prog4webapp/mapper/EmployeeMapper.java
@@ -1,24 +1,30 @@
 package com.web.prog4webapp.mapper;
 
+import com.web.prog4webapp.controller.rest.RestEmployee;
 import com.web.prog4webapp.model.Employee;
 import lombok.AllArgsConstructor;
 import org.hibernate.boot.model.relational.Sequence;
 import org.springframework.stereotype.Component;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.sql.Timestamp;
 import java.time.LocalDate;
+import java.util.Base64;
 
 @Component
 @AllArgsConstructor
 public class EmployeeMapper {
 
-    public Employee toDomain(String lastName, String firstName, LocalDate birthDate, String image) {
+    public Employee toDomain(RestEmployee restEmployee) throws IOException {
+        MultipartFile file = restEmployee.getImage();
+        byte[] bytes = file.getBytes();
+        String image = Base64.getEncoder().encodeToString(bytes);
         Employee newEmployee = new Employee();
-        newEmployee.setLastName(lastName);
-        newEmployee.setFirstName(firstName);
-        newEmployee.setBirthDate(birthDate);
+        newEmployee.setLastName(restEmployee.getLastName());
+        newEmployee.setFirstName(restEmployee.getFirstName());
+        newEmployee.setBirthDate(restEmployee.getBirthDate());
         newEmployee.setImage(image);
-        newEmployee.setMatricule("EMP");
         return newEmployee;
     }
 }
