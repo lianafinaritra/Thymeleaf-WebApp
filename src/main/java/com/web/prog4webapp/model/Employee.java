53d8dfe5a1cb1496a73fa92d44515077c6c2051b
lianafinaritra <hei.lianafinaritra@gmail.com>
2023-07-13 20:46:53 +0300

    chore: update employee model

--- src/main/java/com/web/prog4webapp/model/Employee.java
+++ src/main/java/com/web/prog4webapp/model/Employee.java
@@ -1,30 +1,27 @@
 package com.web.prog4webapp.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import jakarta.persistence.*;
+import lombok.*;
 
 import java.io.Serializable;
 import java.time.LocalDate;
-import java.util.Date;
 
 @AllArgsConstructor
 @Getter
 @Setter
 @NoArgsConstructor
 @Entity
+@EqualsAndHashCode
+@ToString
+@Builder
 public class Employee implements Serializable {
     @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private String id;
     private String matricule;
     private String lastName;
     private String firstName;
     private LocalDate birthDate;
+    @Lob
     private String image;
 }
